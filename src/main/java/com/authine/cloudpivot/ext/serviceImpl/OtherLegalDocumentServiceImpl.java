124d4f5a018d689858391eb400da4fe92def79b2
49994 <499949688@qq.com>
2019-09-02 08:39:09 +0800

    法务模块数据列表接口编写添加URL

--- src/main/java/com/authine/cloudpivot/ext/serviceImpl/OtherLegalDocumentServiceImpl.java
+++ src/main/java/com/authine/cloudpivot/ext/serviceImpl/OtherLegalDocumentServiceImpl.java
@@ -26,9 +26,30 @@ public class OtherLegalDocumentServiceImpl implements OtherLegalDocumentService 
         int pageSize = queryOtherLegalDocument.getPageSize() == 0?10:queryOtherLegalDocument.getPageSize();
         PageHelper.startPage(pageNum, pageSize);
         List<OtherLegalDocumentVO> otherLegalDocumentList = otherLegalDocumentMapper.getOtherLegalDocumentList(queryOtherLegalDocument);
+        for (OtherLegalDocumentVO otherLegalDocumentVO:otherLegalDocumentList){
+            otherLegalDocumentVO.setFormUrl(getFormUrl(otherLegalDocumentVO.getId()));
+        }
         PageInfo<OtherLegalDocumentVO> clientContractVOPageInfo = new PageInfo<>(otherLegalDocumentList);
         PageResult pageResult = PageUtils.getPageResult(clientContractVOPageInfo);
         return pageResult;
-
     }
+
+
+    private String getFormUrl(String id){
+        String ip = "47.103.123.171";
+        StringBuffer fromUrl = new StringBuffer();
+        fromUrl.append("http://");
+        fromUrl.append(ip);
+        fromUrl.append("/form/detail?");
+        fromUrl.append("sheetCode=OtherLegalDocument");
+        fromUrl.append("&objectId=").append(id);
+        fromUrl.append("&schemaCode=OtherLegalDocument");
+        fromUrl.append("&return=/application/ProjectSummary/application-list/OtherLegalDocument?parentId=2c93208b6c9e0bc6016c9e36d7ac0011");
+        fromUrl.append("&code=OtherLegalDocument");
+        fromUrl.append("&openMode");
+        fromUrl.append("&pcUrl");
+        return fromUrl.toString();
+    }
+
+
 }
