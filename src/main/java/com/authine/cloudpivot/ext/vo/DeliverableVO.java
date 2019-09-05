f40cc45b1703161cfd10781966c81e9cc85e048b
DragonTowers <2891879302@qq.com>
2019-09-05 15:06:07 +0800

    deliviable数据接口查询修改

--- src/main/java/com/authine/cloudpivot/ext/vo/DeliverableVO.java
+++ src/main/java/com/authine/cloudpivot/ext/vo/DeliverableVO.java
@@ -4,6 +4,8 @@ package com.authine.cloudpivot.ext.vo;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 
 @Setter
 @Getter
@@ -18,19 +20,19 @@ public class DeliverableVO {
     //交付品类
     private String DeliverableCategory;
     //客户价格(税前)
-    private String ClientPrice;
+    private BigDecimal ClientPrice;
     //单位成本 (税前)
-    private String UnitCost;
+    private BigDecimal UnitCost;
     //数量
-    private String Quantity;
+    private BigDecimal Quantity;
    //主表bigobjectid
     private String bizid;
     //子表每行数据的id
     private String id;*/
     //客户总价格
-    private  String sumClientPrice;
+    private BigDecimal sumClientPrice;
     //单位总成本
-    private String sumUnitCost;
+    private BigDecimal sumUnitCost;
 
 
 
