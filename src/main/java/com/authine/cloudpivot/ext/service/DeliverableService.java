b3bcc1ea41cf8d21c0fff0cebbdadc2a2c227d13
DragonTowers <2891879302@qq.com>
2019-09-05 10:00:25 +0800

    Deliverable表总价格总成本查询接口

---
+++ src/main/java/com/authine/cloudpivot/ext/service/DeliverableService.java
@@ -0,0 +1,13 @@
+package com.authine.cloudpivot.ext.service;
+
+import com.authine.cloudpivot.ext.queryVo.QueryClientPayment;
+import com.authine.cloudpivot.ext.queryVo.QueryDeliverable;
+import com.authine.cloudpivot.ext.vo.DeliverableVO;
+import com.authine.cloudpivot.ext.vo.PageResult;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface DeliverableService {
+    public DeliverableVO getDeliverableList();
+}
