14179ad75f9d0211bc111bbbbebe7a6e2621b818
laixh <laixh@authine.com>
2019-09-10 17:31:31 +0800

    关联接口

--- src/main/java/com/authine/cloudpivot/ext/vo/StagePaymentVO.java
+++ src/main/java/com/authine/cloudpivot/ext/vo/StagePaymentVO.java
@@ -3,9 +3,7 @@ package com.authine.cloudpivot.ext.vo;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.poi.hpsf.Decimal;
 
-import java.util.Date;
 @Getter
 @Setter
 public class StagePaymentVO {
@@ -16,17 +14,17 @@ public class StagePaymentVO {
      @ApiModelProperty("供应商发票号")
      private  String vendorInvoice;
      @ApiModelProperty("供应商开票日期")
-     private Date vendorInvoicingDate ;
+     private String vendorInvoicingDate ;
      @ApiModelProperty("付款")
      private String installment;
      @ApiModelProperty("向供应商付款日期")
-     private Date paymentVendorDate ;
+     private String paymentVendorDate ;
      @ApiModelProperty("参数")
      private  String index ;
      @ApiModelProperty("预计付款日期")
-     private Date paymentCheckDate;
+     private String paymentCheckDate;
      @ApiModelProperty("实际付款日期")
-     private Date actualPaymentDate;
+     private String  actualPaymentDate;
      @ApiModelProperty("关联供应商付款，保存子表数据时使用")
      private String parentId;
 }
